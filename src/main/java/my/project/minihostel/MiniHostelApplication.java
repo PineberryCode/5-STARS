243ce1399c42abcc77297da239185bcc110e9bd0
PineberryCode <applepie090902@gmail.com>
2023-07-31 14:25:24 -0500

    a: from spring

---
+++ src/main/java/my/project/minihostel/MiniHostelApplication.java
@@ -0,0 +1,13 @@
+package my.project.minihostel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniHostelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniHostelApplication.class, args);
+	}
+
+}
